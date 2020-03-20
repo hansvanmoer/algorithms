7ac974391325535366aab6c21e3f93c08b328e6c
hans.vanmoer <hans.vanmoer@formica.digital>
2020-03-20 11:09:41 +0100

    Initial setup of project

---
+++ main.c
@@ -0,0 +1,28 @@
+/**
+ * This file is part of Algorithms.
+ *
+ * Algorithms is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Algorithms is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Algorithms.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+/**
+ * The main application entry point
+ * Tests the relevant algorithms for correctness
+ * @param arg_count the number of command line arguments
+ * @param args the command line arguments
+ * @return the exit code
+ */
+int main(int arg_count, const char ** args){
+  return 0;
+}
